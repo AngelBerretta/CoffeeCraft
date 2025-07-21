735ac01827bd6413ae1dd7e1ae911762998df6a1
Angel <angelcursodeingles2@gmail.com>
2025-07-21 10:20:30 -0300

    proyecto web estático

---
+++ db.js
@@ -0,0 +1,50 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Espresso Clásico",
+        descripcion: "Café espresso intenso, elaborado con granos 100% arábica de origen único",
+        precio: 3.99,
+        imagen: "img/espresso-clasico.jpg",
+        categoria: "espresso"
+    },
+    {
+        id: 2,
+        nombre: "Cold Brew Nitro",
+        descripcion: "Café frío infusionado con nitrógeno, suave y cremoso",
+        precio: 5.49,
+        imagen: "img/cold-brew-nitro.jpg",
+        categoria: "coldbrew"
+    },
+    {
+        id: 3,
+        nombre: "Croissant de Almendra",
+        descripcion: "Croissant crujiente relleno de crema de almendra artesanal",
+        precio: 4.49,
+        imagen: "img/croissant-almendra.jpg",
+        categoria: "pasteleria"
+    },
+    {
+        id: 4,
+        nombre: "Cappuccino",
+        descripcion: "Espresso con leche vaporizada y espuma cremosa",
+        precio: 4.99,
+        imagen: "img/cappuccino.jpg",
+        categoria: "espresso"
+    },
+    {
+        id: 5,
+        nombre: "Cold Brew Vainilla",
+        descripcion: "Café frío con un toque de vainilla natural",
+        precio: 5.99,
+        imagen: "img/cold-brew-vainilla.jpg",
+        categoria: "coldbrew"
+    },
+    {
+        id: 6,
+        nombre: "Tarta de Chocolate",
+        descripcion: "Tarta artesanal de chocolate oscuro con un toque de café",
+        precio: 6.49,
+        imagen: "img/tarta-chocolate.jpg",
+        categoria: "pasteleria"
+    }
+];
